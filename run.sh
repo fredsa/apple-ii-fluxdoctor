d7aff2243d06f74dcdc3bafd63f13466a0bbe002
Fred Sauer <fredsa@google.com>
2026-08-28 22:42:43 -0700

    tape file support

--- run.sh
+++ run.sh
@@ -63,62 +63,121 @@ if [[ ! -x "$AC" ]]; then
     exit 1
 fi
 
-echo "Creating blank image:"
-echo "  images/hello.do -> fluxdoctor.do"
-rm -f fluxdoctor.do
-# $AC -dos140 fluxdoctor.do
-cp images/hello.do fluxdoctor.do
-# ls -l fluxdoctor.do
+if ! type -p fluxrider >/dev/null 2>&1; then
+    echo "ERROR missing executable:" 1>&2
+    echo "  fluxrider" 1>&2
+    echo "" 1>&2
+    echo "Please install fluxrider from:" 1>&2
+    echo "  https://bitbucket.org/fredsa/fluxrider" 1>&2
+    exit 1
+fi
 
+echo "====================================================================="
+echo "Cleanup output folder: out/"
+mkdir -p out
+rm -f out/fluxdoctor.do
+rm -f out/fluxdoctor.bin
+rm -f out/disk-pgm.bin
+rm -f out/fluxdoctor-tape.asm
+rm -f out/fluxdoctor-tape.bin
+rm -f out/fluxdoctor-tape.wav
+
+echo
+echo "====================================================================="
+echo "Creating single file tape program:"
+cat fluxdoctor.asm \
+    | sed -e 's#pgmstart *equ *$2000#pgmstart equ $080d#' \
+    > out/fluxdoctor-tape.asm
+if cmp -s fluxdoctor.asm out/fluxdoctor-tape.asm
+then
+    echo "Failed to modify start address" 1>&2
+    exit 1
+fi
+
+echo
+echo "====================================================================="
 echo "Compiling:"
-echo "  fluxdoctor.asm -> fluxdoctor.bin"
+echo "  fluxdoctor.asm -> out/fluxdoctor.bin"
+echo "  out/fluxdoctor-tape.asm -> out/fluxdoctor-tape.bin"
 if [[ ! -r fluxdoctor.asm ]]; then
     echo "MISSING: fluxdoctor.asm" 1>&2
     exit 1
 fi
-rm -f fluxdoctor.bin
-dasm fluxdoctor.asm -f3 -ofluxdoctor.bin
-# ls -l fluxdoctor.bin
+dasm fluxdoctor.asm -f3 -oout/fluxdoctor.bin
+echo "  out/fluxdoctor-tape.asm -> out/fluxdoctor-tape.bin"
+dasm out/fluxdoctor-tape.asm -f3 -oout/fluxdoctor-tape.bin
+# ls -l out/fluxdoctor.bin out/fluxdoctor-tape.bin
 
-rm -f pgm.bin
-echo "Extracting binary (removing 4 byte header):"
-echo "  fluxdoctor.bin -> pgm.bin"
+echo
+echo "====================================================================="
+echo "Extracting disk binary (removing 4 byte header):"
+echo "  out/fluxdoctor.bin -> out/disk-pgm.bin"
 # Get start address in hex from first two byte.
-startadr="0x$(od -An -t x2 -N2 fluxdoctor.bin | tr -d ' ')"
-echo "Start address: $startadr"
-# Remove first four bytes (len + start)
-cat fluxdoctor.bin | tail -c+5 > pgm.bin
+startaddr="0x$(od -An -t x2 -N2 out/fluxdoctor.bin | tr -d ' ')"
+echo "Start address: $startaddr"
+# Remove first four bytes (start + len)
+cat out/fluxdoctor.bin | tail -c+5 > out/disk-pgm.bin
 
-echo "Removing existing program:"
+echo
+echo "====================================================================="
+echo "Creating blank disk image:"
+echo "  template/hello.do -> out/fluxdoctor.do"
+# $AC -dos140 out/fluxdoctor.do
+cp template/hello.do out/fluxdoctor.do
+# ls -l out/fluxdoctor.do
+
+echo
+echo "====================================================================="
+echo "Removing FLUXDOCTOR from template disk image:"
 echo "  FLUXDOCTOR"
-$AC -d fluxdoctor.do FLUXDOCTOR
+$AC -d out/fluxdoctor.do FLUXDOCTOR
 
+echo
+echo "====================================================================="
 echo "Adding binary to disk image:"
-echo "  pgm.bin -> FLUXDOCTOR"
-$AC -p fluxdoctor.do FLUXDOCTOR B $startadr < pgm.bin
+echo "  out/disk-pgm.bin -> FLUXDOCTOR"
+$AC -p out/fluxdoctor.do FLUXDOCTOR B $startaddr < out/disk-pgm.bin
 
-echo "Final disk image:"
-echo "  fluxdoctor.do"
-$AC -ll fluxdoctor.do
+echo
+echo "====================================================================="
+echo "Final disk image is ready:"
+echo "  out/fluxdoctor.do"
+$AC -ll out/fluxdoctor.do
 
+echo
+echo "====================================================================="
+echo "Creating out/fluxdoctor-tape.bin"
+# `42 CALL 2061`
+printf '\x0B\x08\x2A\x00\x8C\x32\x30\x36\x31\x00\x00\x00' > out/fluxdoctor-tape.bin
+cat out/disk-pgm.bin >> out/fluxdoctor-tape.bin
+
+echo
+echo "====================================================================="
+echo "Creating cassette bootable WAV file:"
+echo "  out/fluxdoctor-tape.bin -> out/fluxdoctor-tape.wav"
+fluxrider out/fluxdoctor-tape.bin out/fluxdoctor-tape.wav
+
+echo
+echo "====================================================================="
 echo "To write image to a physical floppy using greaseweazle:"
-echo "  gw write --tracks=step=2 fluxdoctor.do    # 96TPI floppy drive"
-echo "  gw write fluxdoctor.do                    # 48TPI floppy drive"
+echo "  gw write --tracks=step=2 out/fluxdoctor.do    # 96TPI floppy drive"
+echo "  gw write out/fluxdoctor.do                    # 48TPI floppy drive"
 
 os_name="$(uname -s)"
 case $os_name in
   Darwin)
     # macOS
-    '/Applications/Virtual ][.app/Contents/MacOS/Virtual ][' fluxdoctor.do
+    '/Applications/Virtual ][.app/Contents/MacOS/Virtual ][' out/fluxdoctor.do
+    # '/Applications/Virtual ][.app/Contents/MacOS/Virtual ][' out/fluxdoctor-tape.wav
     ;;
   Linux)
     # Linux
-    echo "TODO: launch emulator with disk image: fluxdoctor.do"
+    echo "TODO: launch emulator with disk image: out/fluxdoctor.do"
     ;;
   MINGW64*)
     # Windows Git Bash
     echo "Launching AppleWin emulator"
-    AppleWin -d1 fluxdoctor.do
+    AppleWin -d1 out/fluxdoctor.do
     ;;
   *)
     echo "ERROR: Unknown OS name for `$os_name`" 1>&2
